61a8770c6acc8725e53f4802e5a7525b02a57b68
Joseph Assouline <joseph@getluko.com>
2020-04-14 00:53:38 +0200

    fit France commit

--- model_epidemio/covid_data.py
+++ model_epidemio/covid_data.py
@@ -12,8 +12,8 @@ def make_data(death, cases, init, delta):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     size = len(cases[init:])
     t = torch.linspace(0.0, size - 1, size)
-    data_death = torch.tensor(death[init:], device=device)
-    data_death = data_death.type(torch.FloatTensor, device=device)
-    data_cases = torch.tensor(cases[init - delta : -delta], device=device)
-    data_cases = data_cases.type(torch.FloatTensor, device=device)
+    data_death = torch.tensor(death[init:]).to(device)
+    data_death = data_death.type(torch.FloatTensor).to(device)
+    data_cases = torch.tensor(cases[init - delta : -delta]).to(device)
+    data_cases = data_cases.type(torch.FloatTensor).to(device)
     return t, data_death, data_cases
